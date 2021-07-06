5437b3a4bfc9e85947dcc290b19eb0a76f39cfc4
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-07-06 20:03:50 +0530

    Added About.html and linked it to View.py pipeline

--- ecom_web/shop/views.py
+++ ecom_web/shop/views.py
@@ -1,4 +1,25 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
 def index(request):
     return render(request,'shop1/index.html')
+
+def about(request):
+    return render(request,'shop1/about.html')
+
+def contact(request):
+    return HttpResponse("We are at contact")
+
+def tracker(request):
+    return HttpResponse("We are at tracker")
+
+def productview(request):
+    return HttpResponse("We are at product view")
+
+def search(request):
+    return HttpResponse("We are at search")
+
+def checkout(request):
+    return HttpResponse("We are at checkout")
+
+
